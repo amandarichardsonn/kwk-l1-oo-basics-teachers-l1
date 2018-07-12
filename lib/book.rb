de5af46b91e3330e7186291fde27f7d485f24377
Amanda Richardson <amanda162000@gmail.com>
2018-07-12 03:26:43 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,19 +1,21 @@
 # book.rb
+ 
 class Book
-  # the method that we can pass arguments to when initializing a new instance of a class.
-
+ 
   def initialize(title)
     @title = title
   end
-
+ 
   def title
-     @title
-    end
-    def author=(author)
-   @author = author
-  #  Setters are methods that allow us to set instance variables. It seems weird, but they all are named like this: property=. 
-
-
-
- end
+    @title
+  end
+ 
+  def author=(author)
+    @author = author
+  end
+ 
+  def author
+    @author
+  end
+ 
 end
