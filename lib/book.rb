6abd19aef2fd35acdaec7161ec13d967d91d034d
Amanda Richardson <amanda162000@gmail.com>
2018-07-12 03:13:22 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,7 +1,10 @@
 # book.rb
 class Book
- 
+  # the method that we can pass arguments to when initializing a new instance of a class.
+
   def initialize(title)
+    @title = title
   end
- 
+  def title
+    end
 end
